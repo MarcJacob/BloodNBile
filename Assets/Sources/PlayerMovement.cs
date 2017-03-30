1f11a6d8f184dc0b2e48d72ba6f75a885bdc2249
Schenger <manu-26@live.fr>
2017-03-30 20:47:17 +0200

    Basics Movement + Camera

---
+++ Assets/Sources/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Animator animations;
+
+
+	void Start () {
+        animations = gameObject.GetComponent<Animator>();
+	}
+	
+	void Update () {
+
+        if (Input.GetKey(KeyCode.Z))
+        {
+            animations.CrossFade("MoveForward", 0.1f);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            animations.CrossFade("MoveBack", 0.1f);
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            animations.CrossFade("MoveLeft", 0.1f);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            animations.CrossFade("MoveRight", 0.1f);
+        }
+
+
+	}
+}
