1b2be63da123a9c20f65d2b49fbea6f7d00ac915
Imonor <ilan.piperno@gmail.com>
2017-04-15 17:35:29 +0200

    MAJ - Master build du <dateDuDernierMasterBuild>

---
+++ Assets/Sources/Entities/EntityManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityManager : MonoBehaviour {
+
+    Mage mage;
+
+    // Use this for initialization
+    void Start () {
+        HumorLevels loul = new HumorLevels(50, 50, 50, 50);
+        ConvertSpell BloodToPhlegm = new ConvertSpell(0, 20, 5, 1);
+        ConvertSpell BloodToBlack = new ConvertSpell(0, 20, 5, 2);
+        ConvertSpell BloodToYelllow = new ConvertSpell(0, 20, 5, 3);
+        ConvertSpell PhlegmToBlood = new ConvertSpell(1, 20, 5, 0);
+        ConvertSpell PhlegmToBlack = new ConvertSpell(1, 20, 5, 2);
+        ConvertSpell PhlegmToYellow = new ConvertSpell(1, 20, 5, 3);
+        ConvertSpell BlackToBlood = new ConvertSpell(2, 20, 5, 0);
+        ConvertSpell BlackToPhlegm = new ConvertSpell(2, 20, 5, 1);
+        ConvertSpell BlackToYellow = new ConvertSpell(2, 20, 5, 3);
+        ConvertSpell YellowToBlood = new ConvertSpell(3, 20, 5, 0);
+        ConvertSpell YellowToPhlegm = new ConvertSpell(3, 20, 5, 1);
+        ConvertSpell YellowToBlack = new ConvertSpell(3, 20, 5, 2);
+
+        mage = new Mage(Vector3.zero, Quaternion.identity, "Loul", loul);
+
+        mage.Cast(BloodToPhlegm);
+        mage.Cast(PhlegmToBlack);
+        mage.Cast(YellowToBlack);
+        mage.Cast(BloodToBlack);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        UpdateEntities();
+        mage.UpdateCooldowns();
+	}
+
+    private void UpdateEntities()
+    {
+        foreach(Entity e in Entity.GetAllEntities())
+        {
+            e.UpdateEntity();
+        }
+    }
+}
---
+++ Assets/Sources/Spells/Spell.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spell {
+
+    public int ID { get; protected set; }
+    private static int LastID = 0;
+    public int Humor { get; protected set; }
+    public int Cost { get; protected set; }
+    public float Cooldown { get; protected set; }
+    public bool IsReloading { get; protected set; }
+    public Effect SpellEffect { get; protected set; }
+    public static List<Spell> SpellsList = new List<Spell>();
+
+    public Spell(int humor, int cost, int cooldown)
+    {
+        Humor = humor;
+        Cost = cost;
+        Cooldown = cooldown;
+        IsReloading = false;
+        ID = LastID;
+        LastID++;
+        SpellsList.Add(this);
+    }
+
+    public bool IsCastable(Mage mage)
+    {
+        bool isCastable = true;
+        if (IsReloading) isCastable = false;
+        else
+        {
+            switch (Humor)
+            {
+                case 0: if (mage.Humors.Blood < Cost) isCastable = false; break;
+                case 1: if (mage.Humors.Phlegm < Cost) isCastable = false; break;
+                case 2: if (mage.Humors.BlackBile < Cost) isCastable = false; break;
+                case 3: if (mage.Humors.YellowBile < Cost) isCastable = false; break;
+            }
+        }
+        return isCastable;
+    }
+
+    public virtual void Cast(Mage caster)
+    {
+        IsReloading = true;
+        Debug.Log("Je me lance !");
+    }
+
+    public void HasReloaded()
+    {
+        IsReloading = false;
+        Debug.Log("Wow je suis prêt mtn");
+    }
+}
