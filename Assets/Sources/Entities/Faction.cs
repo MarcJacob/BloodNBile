629f71725f2f8c2c08561208102cde81b6032a66
MarcJacob <marcjacobpro@gmail.com>
2017-05-24 08:57:00 +0200

    Fix de la Serialization Loop - Faction.cs

--- Assets/Sources/Entities/Faction.cs
+++ Assets/Sources/Entities/Faction.cs
@@ -9,8 +9,6 @@ public class Faction {
     string FactionName;
     int FactionColor = 0;  // 0 = no faction, 1 = red, 2 = blue (ceci est un exemple en vrai on s'en bat les couilles des couleurs)
 
-    List<Mage> Mages; // Ensemble des mages faisant partie de cette faction.
-
     public Faction(string name, int color)
     {
         FactionName = name;
@@ -21,20 +19,5 @@ public class Faction {
     {
         if (this.FactionName == faction.FactionName) return true;
         else return false;
-    }
-
-    public void AddMageToFaction(Mage mage)
-    {
-        if (Mages == null)
-        {
-            Mages = new List<Mage>();
-        }
-
-        Mages.Add(mage);
-    }
-
-    public Mage[] GetMages()
-    {
-        return Mages.ToArray();
     }
 }
